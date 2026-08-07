62dd9ed2a33f4a28c652cd75b6ddba52e6f8c2fb
thomasjudes-del <thomas.judes@gmail.com>
2026-08-07 20:59:19 +0200

    feat: route /green-claims-fix to venture page

--- index.tsx
+++ index.tsx
@@ -1,17 +1,20 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GreenClaimsFix from './src/pages/GreenClaimsFix';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
+
+const pathname = window.location.pathname.replace(/\/$/, '') || '/';
+const Page = pathname === '/green-claims-fix' ? GreenClaimsFix : App;
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
 );
